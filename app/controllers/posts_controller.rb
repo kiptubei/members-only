bcdb4a11c4b7af2cd67b5123ee21fd2fd3f34a3d
OlawaleJoseph <adedekojoseph@gmail.com>
2020-08-13 12:16:47 +0100

    authenticate members before creating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :authenticate_member!, only: %i[new create]
 
   def index
-    @posts = Article.all
+    @posts = Post.all
   end
 
   def new
@@ -12,9 +12,20 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(article_params)
-    @post.save
-    # flash.notice = "Article '#{@article.title}' Created!"
-    redirect_to post_path(@post)
+    @post = Post.new(post_params)
+    @post.member_id = current_member.id
+    if @post.save
+      flash.notice = "Post Created!"
+      redirect_to posts_path
+    else
+      flash['alert'] = @post.errors.full_messages
+      redirect_to new_post_path
+    end
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
